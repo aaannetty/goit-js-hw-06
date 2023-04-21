c142c50713f0ea34cdf56b107dd23e5f9818e579
aaannetty <latsdoit@gmail.com>
2023-04-21 22:27:44 +0300

    Task 05 done

---
+++ js/task-05.js
@@ -0,0 +1,10 @@
+const nameInput = document.querySelector("#name-input");
+const nameOutput = document.querySelector("#name-output");
+
+nameInput.addEventListener("input", (event) => {
+  if (event.target.value !== "") {
+    nameOutput.textContent = event.target.value;
+  } else {
+    nameOutput.textContent = "Anonymous";
+  }
+});
