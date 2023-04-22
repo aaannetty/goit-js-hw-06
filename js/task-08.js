50e4c8459d2d69015e9c375752202d1b84d7051b
aaannetty <latsdoit@gmail.com>
2023-04-22 23:12:32 +0300

    Task 08 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,27 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const emailInput = form.elements.email;
+  const passwordInput = form.elements.password;
+
+  if (emailInput.value === "" || passwordInput.value === "") {
+    alert("All fields must be filled in!");
+    return;
+  }
+
+  const formElements = event.currentTarget.elements;
+  const formData = new FormData(event.currentTarget);
+
+  console.log(formData);
+
+  formData.forEach((value, name) => {
+    console.log("onFormSubmit -> name", name);
+    console.log("onFormSubmit -> value", value);
+  });
+
+  form.reset();
+}
