471761ea889bb8e8ec397ea06d9693c988193dd8
aaannetty <latsdoit@gmail.com>
2023-04-22 23:42:31 +0300

    Task 09 done

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const colorSpan = document.querySelector(".color");
+
+button.addEventListener("click", function () {
+  const color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  colorSpan.textContent = color;
+});
